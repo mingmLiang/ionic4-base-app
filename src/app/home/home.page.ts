73b7f296dd76baa31cef09d2ecf61dbe3618c222
mingmLiang <2324607198@qq.com>
2019-02-22 17:20:06 +0800

    引入自定义字体图标

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+// import { Tab1 } from './tab1-page';
+// import { Tab2 } from './tab2-page';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tab1: any;
+  tab2: any;
 
+  constructor() {
+    // this.tab1 = Tab1;
+    // this.tab2 = Tab2;
+  }
 }
