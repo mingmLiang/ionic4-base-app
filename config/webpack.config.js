6bbaf8cd873aca6de8f54a6f60662edb16f262e8
mingming <2324607198@qq.com>
2019-02-25 21:32:23 +0800

    【C】alias

---
+++ config/webpack.config.js
@@ -0,0 +1,37 @@
+const chalk = require("chalk");
+const fs = require('fs');
+const path = require('path');
+const useDefaultConfig = require('@ionic/app-scripts/config/webpack.config.js');
+
+const webpackConfig = process.env.IONIC_ENV || 'dev';
+const env = require('minimist')(process.argv.slice(2)).env;
+
+console.log(chalk.yellow.bgBlack('\nUsing ' + (!env ? 'DEFAULT' : env.toUpperCase()) + ' environment variables for ' + webpackConfig.toUpperCase() + ' build.\n'));
+
+useDefaultConfig[env].resolve.alias = {
+  "@app": path.resolve('./src/app/'),
+  "@assets": path.resolve('./src/assets/'),
+  "@env": path.resolve(environmentPath(env)),
+  "@pages": path.resolve('./src/pages/'),
+  "@services": path.resolve('./src/services/'),
+  "@tests": path.resolve('./src/'),    
+  "@theme": path.resolve('./src/theme/')
+};
+
+function environmentPath(env) {
+  envFileName = 'environment' + (!env ? '' : '.' + env) + '.ts';
+
+  let filePath = './src/environments/' + envFileName;
+
+  console.log(chalk.yellow.bgBlack('Loading ' + filePath + '\n'));
+
+  if (!fs.existsSync(filePath)) {
+    console.log(chalk.red('\n' + filePath + ' does not exist!'));
+  } else {
+    return filePath;
+  }
+}
+
+module.exports = function () {
+  return useDefaultConfig;
+};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { environment } from '@src/environments/environment';
+import { environment } from '@env';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -15,6 +15,7 @@ export class AppComponent {
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+    console.log(environment);
   }
 
   initializeApp() {
